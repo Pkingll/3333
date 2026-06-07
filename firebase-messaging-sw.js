cf5abc64c61a6d4266e5057d7c2f0225c22c0c2a
Mnasaa <alfzhm9@gmail.com>
2026-06-07 22:29:07 +0200

    366555
    فففف

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,22 +1,13 @@
-/* firebase-messaging-sw.js */
-
 importScripts('https://www.gstatic.com/firebasejs/10.13.2/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/10.13.2/firebase-messaging-compat.js');
 
 firebase.initializeApp({
-
 apiKey: "AIzaSyBG_Jmj4gFcYBs8zN2wTXRLOUg7IlVkhF0",
-
 authDomain: "talibat-ilm.firebaseapp.com",
-
 projectId: "talibat-ilm",
-
 storageBucket: "talibat-ilm.firebasestorage.app",
-
 messagingSenderId: "381987453673",
-
 appId: "1:381987453673:web:085dcae14d1cdccb2ace78"
-
 });
 
 const messaging = firebase.messaging();
