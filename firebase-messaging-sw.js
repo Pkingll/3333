f22867a517cf54af33aef36a8f0ed8e1b890e2eb
Mnasaa <alfzhm9@gmail.com>
2026-06-08 07:30:33 +0200

    Implement Firebase messaging background notifications22
    Add background message handling and notification click event.تت

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,24 +2,58 @@ importScripts('https://www.gstatic.com/firebasejs/10.13.2/firebase-app-compat.js
 importScripts('https://www.gstatic.com/firebasejs/10.13.2/firebase-messaging-compat.js');
 
 firebase.initializeApp({
+
 apiKey: "AIzaSyBG_Jmj4gFcYBs8zN2wTXRLOUg7IlVkhF0",
+
 authDomain: "talibat-ilm.firebaseapp.com",
+
 projectId: "talibat-ilm",
+
 storageBucket: "talibat-ilm.firebasestorage.app",
+
 messagingSenderId: "381987453673",
+
 appId: "1:381987453673:web:085dcae14d1cdccb2ace78"
+
 });
 
 const messaging = firebase.messaging();
 
+/* استقبال الإشعار بالخلفية */
+
 messaging.onBackgroundMessage(function(payload) {
 
 self.registration.showNotification(
+
 payload.notification.title,
+
 {
 body: payload.notification.body,
-icon: 'https://a.top4top.io/p_38068co2c0.jpg'
+
+icon: 'https://a.top4top.io/p_38068co2c0.jpg',
+
+data: {
+url: 'https://pkingll.github.io/'
 }
+
+}
+
+);
+
+});
+
+/* عند الضغط على الإشعار */
+
+self.addEventListener('notificationclick', function(event) {
+
+event.notification.close();
+
+event.waitUntil(
+
+clients.openWindow(
+'https://pkingll.github.io/'
+)
+
 );
 
 });
