8edd96820aad342a2f4722daa2395a242b602474
Mnasaa <alfzhm9@gmail.com>
2026-06-08 10:15:43 +0200

    Refactor firebase-messaging-sw.js for clarity366
    Refactor Firebase messaging service worker to improve readability and structure.

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,92 +1,49 @@
-importScripts('https://www.gstatic.com/firebasejs/10.13.2/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/10.13.2/firebase-messaging-compat.js');
+importScripts(
+'https://www.gstatic.com/firebasejs/10.13.2/firebase-app-compat.js'
+);
+
+importScripts(
+'https://www.gstatic.com/firebasejs/10.13.2/firebase-messaging-compat.js'
+);
 
 firebase.initializeApp({
-  apiKey: "AIzaSyBG_Jmj4gFcYBs8zN2wTXRLOUg7IlVkhF0",
-  authDomain: "talibat-ilm.firebaseapp.com",
-  projectId: "talibat-ilm",
-  storageBucket: "talibat-ilm.firebasestorage.app",
-  messagingSenderId: "381987453673",
-  appId: "1:381987453673:web:085dcae14d1cdccb2ace78",
-  measurementId: "G-SPG9FR05WV"
-});
 
-const messaging = firebase.messaging();
+apiKey:
+"AIzaSyBG_Jmj4gFcYBs8zN2wTXRLOUg7IlVkhF0",
 
-// استقبال الإشعارات بالخلفية
-messaging.onBackgroundMessage((payload) => {
+authDomain:
+"talibat-ilm.firebaseapp.com",
 
-  const notificationTitle =
-    payload.notification?.title || "إشعار جديد";
+projectId:
+"talibat-ilm",
 
-  const notificationOptions = {
+storageBucket:
+"talibat-ilm.firebasestorage.app",
 
-    body:
-      payload.notification?.body || "",
+messagingSenderId:
+"381987453673",
 
-    // أيقونة الإشعار
-    icon:
-      "https://pkingll.github.io/3333/icon-192.png",
-
-    // الشعار الصغير
-    badge:
-      "https://pkingll.github.io/3333/icon-192.png",
-
-    // صورة كبيرة داخل الإشعار
-    image:
-      payload.notification?.image ||
-      "https://pkingll.github.io/3333/icon-512.png",
-
-    vibrate: [200, 100, 200],
-
-    requireInteraction: true,
-
-    data: {
-      url:
-        "https://pkingll.github.io/3333/"
-    }
-
-  };
-
-  self.registration.showNotification(
-    notificationTitle,
-    notificationOptions
-  );
+appId:
+"1:381987453673:web:085dcae14d1cdccb2ace78"
 
 });
 
-// عند الضغط على الإشعار
-self.addEventListener('notificationclick', function(event) {
+const messaging =
+firebase.messaging();
 
-  event.notification.close();
+messaging.onBackgroundMessage(function(payload){
 
-  const urlToOpen =
-    event.notification.data.url;
+self.registration.showNotification(
 
-  event.waitUntil(
+payload.notification.title,
 
-    clients.matchAll({
-      type: 'window',
-      includeUncontrolled: true
-    }).then((clientList) => {
+{
+body: payload.notification.body,
 
-      // إذا الموقع مفتوح يركز عليه
-      for (const client of clientList) {
+icon:
+'https://a.top4top.io/p_38068co2c0.jpg'
+}
 
-        if (client.url.includes(urlToOpen) &&
-            'focus' in client) {
-
-          return client.focus();
-        }
-      }
-
-      // إذا مغلق يفتحه
-      if (clients.openWindow) {
-        return clients.openWindow(urlToOpen);
-      }
-
-    })
-
-  );
+);
 
 });
