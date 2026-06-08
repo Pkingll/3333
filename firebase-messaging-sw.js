f5fe1b75ac64092b34740247187ae969c2cc35ec
Mnasaa <alfzhm9@gmail.com>
2026-06-08 09:49:52 +0200

    Improve background message and notification handling222
    Refactor background message handling and notification click eعععvent.

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -13,23 +13,17 @@ firebase.initializeApp({
 
 const messaging = firebase.messaging();
 
-// استقبال الإشعار بالخلفية
-messaging.onBackgroundMessage(function(payload) {
-
-  console.log("رسالة بالخلفية:", payload);
+messaging.onBackgroundMessage((payload) => {
 
   const notificationTitle =
     payload.notification?.title || "إشعار جديد";
 
   const notificationOptions = {
-    body: payload.notification?.body || "",
-    icon: "/icon-192.png",
-    badge: "/icon-192.png",
-    data: {
-      url:
-        payload.data?.url ||
-        "https://talibat-ilm.web.app/"
-    }
+    body:
+      payload.notification?.body || "",
+    icon:
+      payload.notification?.icon ||
+      "/icon-192.png"
   };
 
   self.registration.showNotification(
@@ -39,40 +33,15 @@ messaging.onBackgroundMessage(function(payload) {
 
 });
 
-// الضغط على الإشعار
+// عند الضغط على الإشعار
 self.addEventListener('notificationclick', function(event) {
 
   event.notification.close();
 
-  const urlToOpen =
-    event.notification.data.url;
-
   event.waitUntil(
-
-    clients.matchAll({
-      type: 'window',
-      includeUncontrolled: true
-    }).then(function(clientList) {
-
-      // إذا الموقع مفتوح
-      for (let i = 0; i < clientList.length; i++) {
-
-        let client = clientList[i];
-
-        if (client.url.includes(urlToOpen) &&
-            'focus' in client) {
-
-          return client.focus();
-        }
-      }
-
-      // فتح الموقع
-      if (clients.openWindow) {
-        return clients.openWindow(urlToOpen);
-      }
-
-    })
-
+    clients.openWindow(
+      'https://pkingll.github.io/3333/'
+    )
   );
 
 });
